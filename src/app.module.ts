68766513d874c197635481c902f6afc9c0d3495c
Rafael Feiten <rafael.feiten@paipe.co>
2022-11-07 15:27:01 -0300

    typeorm and sqlite config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,7 +1,17 @@
 import { Module } from '@nestjs/common';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { PersonModule } from './modules/person.module';
 
 @Module({
-  imports: [PersonModule],
+  imports: [
+    PersonModule,
+    //@todo import config from json file
+    TypeOrmModule.forRoot({
+      database: './db.sql',
+      type: 'sqlite',
+      synchronize: true,
+      entities: ['dist/**/*.model{.ts,.js}'],
+    }),
+  ],
 })
 export class AppModule {}
