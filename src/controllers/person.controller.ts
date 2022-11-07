ebdab58b027585ff1e7f1dfb44e2eac257342d93
Rafael Feiten <rafael.feiten@paipe.co>
2022-11-07 15:29:25 -0300

    person creat read update and delete

--- src/controllers/person.controller.ts
+++ src/controllers/person.controller.ts
@@ -1,29 +1,72 @@
-import { Controller, Delete, Get, Post, Put } from '@nestjs/common';
+import {
+  Body,
+  Controller,
+  Delete,
+  Get,
+  NotFoundException,
+  Param,
+  ParseIntPipe,
+  Post,
+  Put,
+} from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { PersonModel } from 'src/models/person.model';
+import { PersonSchema } from 'src/schemas/person.schema';
+import { Repository } from 'typeorm';
 
 @Controller('/person')
 export class PersonController {
+  constructor(
+    @InjectRepository(PersonModel) private model: Repository<PersonModel>
+  ) {}
+
   @Post()
-  public create(): any {
-    return { data: 'Created!' };
+  public async create(@Body() body: PersonSchema): Promise<PersonModel> {
+    const personCreated = await this.model.save(body);
+    return personCreated;
   }
 
   @Get()
-  public getAll(): any {
-    return { data: 'GetAll!' };
+  public async getAll(): Promise<PersonModel[]> {
+    const list = await this.model.find();
+    return list;
   }
 
   @Get(':id')
-  public getOne(): any {
-    return { data: 'GetOne!' };
+  public async getOne(
+    @Param('id', ParseIntPipe) id: number
+  ): Promise<PersonModel> {
+    const person = await this.model.findOne({ where: { id } });
+    if (!person) {
+      throw new NotFoundException('Person not found.');
+    }
+    return person;
   }
 
   @Put(':id')
-  public update(): any {
-    return { data: 'Update!' };
+  public async update(
+    @Param('id', ParseIntPipe) id: number,
+    @Body() body: PersonSchema
+  ): Promise<{ message: string }> {
+    const person = await this.model.findOne({ where: { id } });
+    if (!person) {
+      throw new NotFoundException('Person not found.');
+    }
+
+    await this.model.update({ id }, body);
+    return { message: 'Person updated.' };
   }
 
   @Delete(':id')
-  public delete(): any {
-    return { data: 'Deleted!' };
+  public async delete(
+    @Param('id', ParseIntPipe) id: number
+  ): Promise<{ message: string }> {
+    const person = await this.model.findOne({ where: { id } });
+    if (!person) {
+      throw new NotFoundException('Person not found.');
+    }
+
+    await this.model.delete({ id });
+    return { message: 'Person deleted.' };
   }
 }
